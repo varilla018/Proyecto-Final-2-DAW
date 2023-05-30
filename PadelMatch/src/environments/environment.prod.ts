7fd9e6082adbeb7388b2db19dbbc01085e72e31a
Varilla018 <amsergiovg@gmail.com>
2023-05-30 21:22:11 +0200

    bbdd lista

--- PadelMatch/src/environments/environment.prod.ts
+++ PadelMatch/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  urlUsuario: 'http://127.0.0.1:3000/usuario/'
+  urlUsuario: 'http://127.0.0.1:3000/usuario/',
+  apiUrl: 'http://localhost:8000/',
+  urlLeague: 'http://127.0.0.1:8000/leagues/',
+  urlPlayer: 'http://127.0.0.1:8000/players/',
+  urlTournaments: 'http://127.0.0.1:8000/tournaments/',
+  urlMatch: 'http://127.0.0.1:8000/match/',
 };
